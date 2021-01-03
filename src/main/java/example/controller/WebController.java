456b6859e2896aa53a9dc369dd456b4bc1531701
gregwhitaker <gwhitake@gmail.com>
2021-01-03 10:15:06 -0800

    Doing work

--- src/main/java/example/controller/WebController.java
+++ src/main/java/example/controller/WebController.java
@@ -1,15 +1,21 @@
 package example.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Controller responsible for serving the video player.
+ */
 @Controller
 public class WebController {
-    private static final Logger LOG = LoggerFactory.getLogger(WebController.class);
 
+    /**
+     * Serves the video player webpage.
+     *
+     * @param model spring mvc model
+     * @return video player webpage
+     */
     @GetMapping("/video")
     public String videoPlayer(Model model) {
         return "index.html";
